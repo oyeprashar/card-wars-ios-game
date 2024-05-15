7d97a4d62b7a532e6efe23f5362d0f28e9cead95
shubhamprashar <shubham.prashar@finbox.in>
2024-05-15 20:28:39 +0530

    Initial Commit

---
+++ card-wars/card_warsApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_warsApp.swift
+//  card-wars
+//
+//  Created by Shubham Prashar on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct card_warsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
